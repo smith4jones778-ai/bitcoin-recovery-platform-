b72fb341fbfe971f0389e6d894e7a710b2f90efa
smith4jones778-ai <smith4jones778@gmail.com>
2026-06-06 10:38:59 +0100

    Update dashboard-firebase.js

--- dashboard-firebase.js
+++ dashboard-firebase.js
@@ -29,27 +29,14 @@ from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
 ===================================================== */
 
 const firebaseConfig = {
-
-    apiKey: "AIzaSyBA_gGITL3vOX_wymV8m3c7h_2kYxUEL0A",
-
-    authDomain: "email-5e336.firebaseapp.com",
-
-    databaseURL:
-    "https://email-5e336-default-rtdb.firebaseio.com",
-
-    projectId: "email-5e336",
-
-    storageBucket: "email-5e336.appspot.com",
-
-    messagingSenderId: "561655323170",
-
-    appId:
-    "1:561655323170:web:42fcf886c34f2d5a16487b",
-
-    measurementId:
-    "G-J92WNBY5RL"
-
-};
+          apiKey: "AIzaSyBA_gGITL3vOX_wymV8m3c7h_2kYxUEL0A",
+          authDomain: "email-5e336.firebaseapp.com",
+          projectId: "email-5e336",
+          storageBucket: "email-5e336.appspot.com",
+          messagingSenderId: "561655323170",
+          appId: "1:561655323170:web:42fcf886c34f2d5a16487b",
+          measurementId: "G-J92WNBY5RL"
+       };
 
 /* =====================================================
    INITIALIZE
